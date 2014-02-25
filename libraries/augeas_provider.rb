0060e1f2bd06cd8f74184327a3f7546371925a48
Nathan Huff <nrhuff@umn.edu>
2014-02-25 16:31:14 -0600

    Continue to flesh out augeas provider

--- libraries/augeas_provider.rb
+++ libraries/augeas_provider.rb
@@ -1,16 +1,173 @@
 require 'chef/provider'
 require 'augeas'
+require 'chef/util/diff'
+require 'strscan'
 
 class Chef
   class Provider
     class Augeas < Chef::Provider
 
-      def initialize(new_resource,run_context=nil,aug_class=Augeas)
+      def initialize(new_resource,run_context=nil)
         super(new_resource,run_context)
       end
-      def in_sync?
+
+      def load_current_resource
         true
       end
+
+      def action_run
+        aug = ::Augeas::open()
+        if need_run?(aug,@new_resource.only_if)
+          parse_changes(@new_resource.changes()).map{|x| execute_change(aug,x)}
+        end
+        aug.close()
+      end
+
+      def need_run?(aug,only_if)
+        if only_if
+          check_guard(parse_args(only_if))
+        end
+        return true
+      end
+
+      def check_guard(guard)
+        case guard[0]
+        when 'get'
+          process_get(guard)
+        end
+      end
+
+      def process_get(guard)
+        path,comp,value = guard[1],guard[2],guard[3]
+        unless ['>','<','<=','>=','==','!=','=~'].include?(comp)
+          raise ArgumentError,"Uknown comparator #{comp}"
+        end
+      end 
+
+      def execute_change(aug,change)
+        case change[0]
+        when 'set'
+          raise ArgumentError,"set takes two args" unless change.length()==3
+          aug.set(change[1],change[2])
+        when 'setm'
+          raise ArgumentError,"setm takes three args" unless change.length()==4
+          aug.setm(change[1],change[2],change[3])
+        when 'rm','remove'
+          raise ArgumentError,"rm takes one argument" unless change.length()==2
+          aug.rm(change[1])
+        when 'clear'
+          raise ArgumentError,"clear takes one argument" unless change.length()==2
+          aug.clear(change[1])
+        when 'clearm'
+          raise ArgumentError,"clearm takes two arguments" unless change.length()==3
+          aug.clearm(change[1],change[2])
+        when 'ins','insert'
+          raise ArgumentError,"insert takes three arguments" unless change.length()==4
+          case change[2]
+          when 'before'
+            before = true
+          when 'after'
+            before = false
+          else
+            raise ArgumentError,'location for insert must be before or after'
+          end
+          aug.insert(change[3],change[1],before)
+        when 'mv','move'
+          raise ArgumentError,"mv takes two arguments" unless change.length()==3
+          aug.mv(change[1],change[2])
+        when 'defvar'
+          raise ArgumentError,"defvar takes two arguments" unless change.length()==3
+          aug.defvar(change[1],change[2])
+        when 'defnode'
+          raise ArgumentError,"defvar takes three arguments" unless change.length()==4
+          aug.defnode(change[1],change[2],change[3])
+        else
+          raise ArgumentError,"Unkown augeas command #{change[0]}"
+        end
+      end
+
+      def parse_changes(changes)
+        changes.map{|x| parse_args(x)}
+      end
+
+      def parse_args(args)
+        ret = []
+        rest = args
+        token = nil
+        while rest != "" 
+          token,rest = parse_token(rest)
+          ret << token
+        end
+        return ret
+      end
+
+      #This is kind of ugly
+      def parse_token(input)
+        state = 'run'
+        stack = []
+        ret = ''
+        sc = StringScanner.new(input)
+        while state == 'run'
+          chr = sc.getch
+          case chr
+          when nil
+            if stack.empty?
+              state = 'halt'
+            else
+              state = 'error'
+            end
+          when /\s/
+            if stack.empty?
+              state = 'halt'
+            else
+              ret << chr
+            end
+          when '['
+            stack.push(chr)
+            ret << chr
+          when ']'
+            if stack.last == '['
+              stack.pop
+              ret << chr
+            else
+              state = 'error'
+            end
+          when '('
+            stack.push(chr)
+            ret << chr
+          when ')'
+            if stack.last == '('
+              stack.pop
+              ret << chr
+            else
+              state = 'error'
+            end
+          when '"'
+            if stack.last == '"'
+              stack.pop
+            else
+              stack.push(chr)
+            end
+            ret << chr
+          when "'"
+            if stack.last == "'"
+              stack.pop
+            else
+              stack.push(chr)
+            end
+            ret << chr
+          else
+           ret << chr
+          end 
+        end
+        if state == 'halt'
+          sc.scan(/\s*/)
+          return [ret,sc.rest]
+        else
+          raise ArgumentError,"Couldn't parse path expresion from #{path}"
+        end
+      end
+
     end
   end
 end
--- test/unit/augeas_provider_test.rb
+++ test/unit/augeas_provider_test.rb
@@ -17,10 +17,164 @@ class AugeasProviderTest < Minitest::Test
     assert_kind_of(Chef::Provider::Augeas, @provider)
   end
 
-  def test_with_no_change_needed
-     
-    @provider.stub :in_sync?, true do
+  def test_set_command
+    @new_resource.changes('set /files/etc/hosts[. = \'127.0.0.1\'][last()+1] localhost.localdomain')
 
+    aug = Minitest::Mock.new
+    aug.expect(:set,true,["/files/etc/hosts[. = '127.0.0.1'][last()+1]",'localhost.localdomain'])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_setm_command
+    @new_resource.changes('setm /files/etc/hosts/* foo localhost.localdomain')
+
+    aug = Minitest::Mock.new
+    aug.expect(:setm,true,["/files/etc/hosts/*",'foo','localhost.localdomain'])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_rm_command
+    @new_resource.changes("rm /files/etc/hosts/[. = 'localhost.localdomain']")
+
+    aug = Minitest::Mock.new
+    aug.expect(:rm,true,["/files/etc/hosts/[. = 'localhost.localdomain']"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_clear_command
+    @new_resource.changes("clear /files/etc/hosts/[. = 'localhost.localdomain']")
+
+    aug = Minitest::Mock.new
+    aug.expect(:clear,true,["/files/etc/hosts/[. = 'localhost.localdomain']"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_clearm_command
+    @new_resource.changes("clearm /files/etc/hosts/[. = 'localhost.localdomain'] *")
+
+    aug = Minitest::Mock.new
+    aug.expect(:clearm,true,["/files/etc/hosts/[. = 'localhost.localdomain']","*"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_ins_before_command
+    @new_resource.changes("ins foo before /files/etc/hosts[last()]")
+
+    aug = Minitest::Mock.new
+    aug.expect(:insert,true,["/files/etc/hosts[last()]","foo",true])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_ins_after_command
+    @new_resource.changes("ins foo after /files/etc/hosts[last()]")
+
+    aug = Minitest::Mock.new
+    aug.expect(:insert,true,["/files/etc/hosts[last()]","foo",false])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+  
+  def test_ins_fail
+    @new_resource.changes("ins foo ter /files/etc/hosts[last()]")
+
+    Augeas.stub(:open, true) do
+     assert_raises(ArgumentError) {@provider.run_action(:run)}
     end
   end
+
+  def test_mv_command
+    @new_resource.changes("mv /foo /bar")
+
+    aug = Minitest::Mock.new
+    aug.expect(:mv,true,["/foo","/bar"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_defvar_command
+    @new_resource.changes("defvar foo /bar")
+
+    aug = Minitest::Mock.new
+    aug.expect(:defvar,true,["foo","/bar"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_defnode_command
+    @new_resource.changes("defnode foo /bar baz")
+
+    aug = Minitest::Mock.new
+    aug.expect(:defnode,true,["foo","/bar","baz"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_nonexistent_command
+    @new_resource.changes("asdf foo /bar baz")
+    Augeas.stub(:open, true) do
+     assert_raises(ArgumentError) {@provider.run_action(:run)}
+    end
+  end
+
+  def test_command_array
+    @new_resource.changes(["defvar foo /bar","rm /bar"])
+
+    aug = Minitest::Mock.new
+    aug.expect(:defvar,true,["foo","/bar"])
+    aug.expect(:rm,true,["/bar"])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
+  def test_get
+    @new_resource.only_if('get /foo == bar')
+
+    aug = Minitest::Mock.new
+    aug.expect(:get,'bar',['/foo'])
+    aug.expect(:close,true)
+    Augeas.stub(:open, aug) do
+      @provider.run_action(:run)
+    end
+    aug.verify
+  end
+
 end
