b9ad4f5b9891a04bb2906840edc976f7ec60d307
Nathan Huff <nrhuff@umn.edu>
2014-03-12 12:39:35 -0500

    Add maintainer info to metatdata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'augeas'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Nathan Huff'
+maintainer_email 'nrhuff@umn.edu'
 license          'All rights reserved'
 description      'Installs/Configures augeas'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
