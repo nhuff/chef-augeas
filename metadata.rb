4fe88898beaf300e71c569ec00117f1d11835d60
Nathan Huff <nrhuff@umn.edu>
2014-05-09 10:34:30 -0500

    Update license in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'augeas'
 maintainer       'Nathan Huff'
 maintainer_email 'nrhuff@umn.edu'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures augeas'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
