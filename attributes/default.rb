c501c4830943ba5e4cd7f836c32edd88f9a4750d
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-05-20 11:26:21 +0200

    Missing pkg-config

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ case node['platform']
 when 'redhat', 'centos', 'scientific', 'fedora', 'suse', 'amazon', 'oracle'
   default['augeas']['packages'] = %w(augeas-devel)
 when 'debian', 'ubuntu'
-  default['augeas']['packages'] = %w(libaugeas-dev ruby1.9.1-dev libxml2-dev)
+  default['augeas']['packages'] = %w(libaugeas-dev ruby1.9.1-dev libxml2-dev pkg-config)
 end
