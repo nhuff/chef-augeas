7f37574276a8f5d010a92db4b992705cd575dc47
Nathan Huff <nrhuff@umn.edu>
2014-05-23 09:56:35 -0500

    Switch to thor-scmversion so jenkins can tag the releases

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nrhuff@umn.edu'
 license          'Apache 2.0'
 description      'Installs/Configures augeas'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 depends 'build-essential'
 
