d6ec7fabfafd65953487819a68ce18f84a8a3cf4
Nathan Huff <nrhuff@umn.edu>
2015-03-31 14:36:36 -0500

    Yak shave to get tests to pass with new serverspec and updated docker images

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 t_file = ''
-t_file = '/etc/default/test' if ['Ubuntu','Debian'].include?(os[:family])
-t_file = '/etc/sysconfig/test' if ['Fedora','RedHat'].include?(os[:family])
+t_file = '/etc/default/test' if ['ubuntu','debian'].include?(os[:family])
+t_file = '/etc/sysconfig/test' if ['fedora','redhat'].include?(os[:family])
 
 describe file(t_file) do
   its(:content) { should match(/^TEST=b$/) }
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,10 +1,8 @@
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
+set :backend, :exec
 RSpec.configure do |c|
   c.before :all do
-    c.path = '/sbin:/usr/sbin'
+    c.path = '/sbin:/usr/sbin:/bin:/usr/bin'
   end
 end
