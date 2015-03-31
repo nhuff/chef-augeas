30a22861e77e2365a5bdde63c3c32506b8326446
Nathan Huff <nrhuff@umn.edu>
2015-03-31 13:36:51 -0500

    Add chefspec resource matcher

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -3,6 +3,7 @@
 # -*- coding: UTF-8 -*-
 #
 if defined?(ChefSpec)
+  ChefSpec.define_matcher :augeas
   def run_augeas(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:augeas, :run, resource_name)
   end
