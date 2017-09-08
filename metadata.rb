f73aa6eed9135acd0045b5bf86501bc8e224dd93
Nathan Huff <nhuff@acm.org>
2017-09-08 13:40:15 -0600

    Update metadata for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,16 @@ maintainer_email 'nrhuff@umn.edu'
 license          'Apache 2.0'
 description      'Installs/Configures augeas'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
-
+if File.exist? File.join(File.dirname(__FILE__), 'VERSION')
+  version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+else
+  version '0.0.1'
+end
+chef_version '>= 12.1'
 supports 'rhel'
 supports 'fedora'
 supports 'ubuntu'
+source_url 'https://github.com/nhuff/chef-augeas'
+issues_url 'https://github.com/nhuff/chef-augeas/issues'
+
+depends 'build-essential'
