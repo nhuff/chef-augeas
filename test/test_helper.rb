b7a11d5e982a82425439715313d3d8587d647861
Nathan Huff <nhuff@acm.org>
2021-03-11 12:48:55 -0700

    Remove some unused tests

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path('../../libraries', __FILE__))
-
-require 'rubygems'
-require 'minitest'
-require 'minitest/autorun'
-require 'chef/resource'
-require 'chef/provider'
-require 'chef/event_dispatch/dispatcher'
-require 'chef/run_context'
